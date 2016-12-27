8196e6965f45548f3d812503533a3e3309a7bf9f
Mateusz Lewandowski <Mateusz_Lewandowski@epam.com>
2016-12-27 10:45:46 +0100

    simple animated list

--- app/AnimatedList.component.jsx
+++ app/AnimatedList.component.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {TransitionMotion, spring} from 'react-motion';
 
-export default props => (
-  <div>
-    {props.list.map(item => (
-      <div key={item}>{item}</div>
-    ))}
-  </div>
+export default ({list}) => (
+  <TransitionMotion
+    willLeave={() => ({h: spring(0)})}
+    willEnter={() => ({h: 0})}
+    styles={list.map(item => ({
+      key: item,
+      data: item,
+      style: {
+        h: spring(18)
+      }
+    }))}
+    >
+    {interpolatedData => (
+      <div>
+        {interpolatedData.map(config => (
+          <div key={config.key} style={{height: config.style.h, overflow: 'hidden'}}>
+            {config.data}
+          </div>
+        ))}
+      </div>
+    )}
+  </TransitionMotion>
 );
